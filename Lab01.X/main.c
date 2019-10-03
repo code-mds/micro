e60dc747e34a2798f9632ac8ada60ee16d7e4bbe
code-mds <43860962+code-mds@users.noreply.github.com>
2019-10-03 21:48:03 +0200

    first empty project

---
+++ Lab01.X/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: massi
+ *
+ * Created on 3. ottobre 2019, 21:46
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p32xxxx.h>
+
+/*
+ * 
+ */
+void main() {
+    while(1) {
+        
+    }
+}
+
