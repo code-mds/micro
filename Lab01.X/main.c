f3d402a0245f840558764ef67b06d0d7b1849568
code-mds <43860962+code-mds@users.noreply.github.com>
2019-10-05 14:29:42 +0200

    general notes

--- Lab01.X/main.c
+++ Lab01.X/main.c
@@ -28,27 +28,43 @@
 
 #define DELAY 1000000 // 1 second
 
+// simulo delay 1 sec
+void delay() {
+    int counter = DELAY;
+    while(counter) {
+        counter--;
+    }
+}
+
+// ANSELx: definisco se ANALOGICO=1 o DIGITAL=0
+// TRISx: definisco se INPUT=1 o OUTPUT=0
+// PORTx: per leggere il valore dei bit del registro
+// LATx: per scrivere bit nel registro
+// CNCON: interrupt (change modification)
+// ODCx: OpenDrain control (non approfondiamo)
+
 /*
- * 
+ * Accende tutti gli 8 led, uno alla volta con un ritardo di 1 secondo
+ * Quando sono tutti accesi li spegne e ricomincia
  */
-void main() {
+void main() {    
+    // set Port A come OUTPUT
+    // la porta A ha 12 pin
+    TRISA = 0x0000; 
     
-    TRISA = 0x0000;
-    int i, counter;
+    int i;
     while(1) {
-        LATA = 0x0000;
+        LATA = 0x0000;  // Metto tutti i bit Port A a 0
             
         for(i= 0; i<8; i++) {
-            // delay 1 sec
-            counter = DELAY;
-            while(counter) {
-                counter--;
-            }
+            delay();
             
             switch(i) {
                 case 0:
-                    LATAbits.LATA0 = 1;
+                    // Setto il singolo bit della Port A a 1
+                    LATAbits.LATA0 = 1;  //A0 corrisponde al LED0
                     break;
+                    
                 case 1:
                     LATAbits.LATA1 = 1;
                     break;
@@ -68,7 +84,7 @@ void main() {
                     LATAbits.LATA6 = 1;
                     break;
                 case 7:
-                    LATAbits.LATA7 = 1;
+                    LATAbits.LATA7 = 1; //A0 corrisponde al LED7
                     break;
             }
         }        
--- README.md
+++ README.md
@@ -4,3 +4,55 @@ Programmazione Microcontrollori
 * IDE: MPLABX
 * Micro: PIC32MX370F512L
 * Board: Digilent Basys MX3
+* GPIO:	General Purpose I/O
+* UART:	Universal Asynchronous Receiver Trasmitter
+
+
+## PIC32MX Notes
+
+| Registry | Description |
+| -------- | ----------- |
+| `ANSELx` | definisco se ANALOGICO=1 o DIGITAL=0 |
+| `TRISx`  | definisco se INPUT=1 o OUTPUT=0 | 
+| `PORTx`  | per leggere il valore dei bit del registro |
+| `LATx`   | per scrivere bit nel registro |
+| `CNCON`  | interrupt (change modification) |
+| `ODCx`   | open drain control (non approfondiamo) |
+
+Where `x` is the name of a port (A, B, C, D, ...)
+
+#### Example: Toggle pin 0 of Port D
+```
+// operatore not: ~
+LATDbits.LATD0 = ~LATDbits.LATD0; 
+
+// uso di INV port register: 0000 0000 0000 0001
+LATDINV = 0x0001
+```
+
+## UART
+| Registry | Description |
+| -------- | ----------- |
+| `UxMODE` | configurazione: ON/OFF, parity, # data bits, stop bits, flow control |
+| `UxSTA`  | stato | 
+| `UxTXREG`  | trasmettere dati |
+| `UxRXREG`  | ricevere dati |
+| `UxBRG`  | baud rate generator |
+
+Where `x` is [1..6] to identify UART1 ... UART6
+
+#### Example: config
+```
+//setto il bit (15) per UART ON/OFF, 1=ON
+U1MODEbits.ON = 1;
+
+// setto i 2 bit (8 e 9) per TX e RX, 00=TX+RX usati
+U1MODEbits.UEN = 0b00; // 0b=binario, 0x esadecimale
+
+// setto i 2 bit (1 e 2) per parita',  01=parita' pari
+U1MODEbits.PDSEL = 0b01; 
+
+// setto il bit (0) per stop bit, 0=1 bit stop   (1=2 bit stops)
+U1MODEbits.STSEL = 0; 
+```
+
